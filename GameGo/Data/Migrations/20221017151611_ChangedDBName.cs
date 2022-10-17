8da9cfad44a9517f553063085c8bf903110158a5
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-10-17 08:16:49 -0700

    changed name of database

---
+++ GameGo/Data/Migrations/20221017151611_ChangedDBName.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameGo.Data.Migrations
+{
+    public partial class ChangedDBName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
