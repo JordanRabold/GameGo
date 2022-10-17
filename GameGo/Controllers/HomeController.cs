c704dfcbdf03a0d00c13e418898e6a44bae4ec38
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-10-17 09:45:33 -0700

    Added authorization to homepage so you need to login to view

--- GameGo/Controllers/HomeController.cs
+++ GameGo/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using GameGo.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace GameGo.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
