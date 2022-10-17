f5db69128d7a10a027c0e61b6f34547b053b6951
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-10-17 16:25:11 -0700

    Removed administrator authorization on all links and just made it Authorize so registered users can access the links

--- GameGo/Controllers/HomeController.cs
+++ GameGo/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace GameGo.Controllers
 {
-    [Authorize(Roles = "Administrator")]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
