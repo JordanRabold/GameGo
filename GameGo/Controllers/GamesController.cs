ef97d2798dfa0f7f02d2c2ec3d0a3e4e68de711d
Jordan Rabold <97548765+JordanRabold@users.noreply.github.com>
2022-11-06 16:52:24 -0800

    Added console to create, update, delete, details, edit, index

--- GameGo/Controllers/GamesController.cs
+++ GameGo/Controllers/GamesController.cs
@@ -56,7 +56,7 @@ namespace GameGo.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Price,ESRB,Description")] Game game)
+        public async Task<IActionResult> Create([Bind("Id,Title,Price,ESRB,Description,Console")] Game game)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace GameGo.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Price,ESRB,Description")] Game game)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Price,ESRB,Description,Console")] Game game)
         {
             if (id != game.Id)
             {
